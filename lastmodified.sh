0bf607bdd8009f9a67fa360dca2a738d18759734
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2021-01-28 13:40:55 -0800

    combine git attributes for cv.html

--- lastmodified.sh
+++ lastmodified.sh
@@ -2,4 +2,4 @@
 
 MJD=$(python -c 'from astropy.time import Time; print(f"{Time.now().mjd:.0f}")')
 DATE=$(date +%Y-%m-%d)
-sed "s/Last updated MJD [0-9][0-9][0-9][0-9][0-9] ([02]0[0-9][0-9]-[01][0-9]-[0-3][0-9])/Last updated MJD $MJD ($DATE)/" $1
+sed "/<\!-- offline -->/d;s/Last updated MJD [0-9][0-9][0-9][0-9][0-9] ([02]0[0-9][0-9]-[01][0-9]-[0-3][0-9])/Last updated MJD $MJD ($DATE)/" $1
